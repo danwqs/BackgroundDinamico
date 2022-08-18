0babb4d4567ddc3d631f9a67ee43f4525378e77d
Administrator <danilomoral40@gmail.com>
2022-08-17 23:46:59 -0300

    Hello World

---
+++ app/src/main/java/com/example/dynamicbackground/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.dynamicbackground;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.mainLayout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2500);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+}
